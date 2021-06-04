507683e90e42e3e61c4abfa56b9263c2dd6e66fc
kira <AhmedKira00@gmail.com>
2021-06-04 23:32:34 +0200

    add get_data() function

--- IT/groups/index.js
+++ IT/groups/index.js
@@ -1,22 +1,32 @@
-function _isEmpty(e) { return !(e == undefined || e == null || e == ""); }
+function _isEmpty(e) { return !(e == undefined || e == null || e == ''); }
+/**
+ * @param {string} URL 
+ * @returns {object}
+ */
+const get_data = async (URL, options = { DIR: ''}) => {
+    var API_HOST = `${window.location.protocol}//${window.location.host}`;
+    if (options.DIR.length>0) API_HOST += `/${options.DIR}`;
+    const res = await (await fetch(`${API_HOST}${URL}`)).json();
+    return res;
+}
 
 (async () => { // add data
-    const res = await (await fetch(`${window.location.protocol}//${window.location.host}/MUST/api/MUST/IT/courses-group.json`)).json();
-    // console.log(res);
+    const res = await get_data(`/api/MUST/IT/courses-group.json`,{DIR: 'MUST'}); 
+    // const res = await get_data(`/api/MUST/IT/courses-group.json`); // for testing
+    console.log(res);
     res.links.forEach((e) => {
         if (e.hidden === true) return;
-
         var one_course = "";
         e.url.forEach((ee) => {
             if (ee.url == undefined || ee.url == null || ee.url == "") return;
             one_course += `<div class="one-link hero-btn gray-btn">
             <a href="${ee.url}" target="_blank">
-            <i class="fa fa-${ee.platform}"></i>${ee.platform } ${!_isEmpty(ee.note) ? "" : "(" + ee.note + ")"}</a> 
+            <i class="fa fa-${ee.platform}"></i>${ee.platform} ${!_isEmpty(ee.note) ? "" : "(" + ee.note + ")"}</a> 
             </div>`;
         });
-        document.querySelector( "#group-container").innerHTML += `<div class="course-col">
+        document.querySelector("#group-container").innerHTML += `<div class="course-col">
             <h3>${e.code}${!_isEmpty(e.name) ? "" : ": " + e.name}</h3>
-            <div class="one-course"> ${one_course == ""? "<p>No data available.</p>": one_course} </div>
+            <div class="one-course"> ${one_course == "" ? "<p>No data available.</p>" : one_course} </div>
             </div>`;
     });
 })();
